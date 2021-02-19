366d0f26565a43bd1409e0127814291804f04043
smithtuka <smithtuka@gmail.com>
2021-02-19 00:08:39 -0600

    ft-items

--- src/main/java/com/galbern/req/service/ItemService.java
+++ src/main/java/com/galbern/req/service/ItemService.java
@@ -10,18 +10,4 @@ import java.util.Set;
 
 public interface ItemService {
 
-    public Item addItem(Item i);
-//    public List<Item> addItems(List<Item> items);
-    public Item findItem(Long id);
-    public Set<Item> findAll();
-
-    public Set<Item> retrieveItems(Stage stage);
-    public Set<Item> retrieveItems(Project project, Stage stage);
-    public Set<Item> retrieveItems(Project project, Set<Stage> stages);
-    public Set<Item> retrieveItems(Project project, LocalDate date);
-
-    public Boolean updateItem(Item iNew);
-    public Set<Item> updateItems(Stage stage);
-    public Set<Item> updateItems(Set<Item> items);
-
 }
