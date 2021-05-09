1264ad6c00b5a8f777b76d14e5d7e153e21ac67b
smithtuka <smithtuka@gmail.com>
2021-05-08 20:05:07 -0500

    fix cors

--- src/main/java/com/galbern/req/security/WebSecurityConfig.java
+++ src/main/java/com/galbern/req/security/WebSecurityConfig.java
@@ -128,14 +128,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
 //        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
+//        config.addAllowedHeader("*");
+        config.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type"));
         config.addAllowedMethod("*");
         config.setMaxAge(600l);
 //        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedOriginPatterns(List.of("http://localhost:3001", "http://localhost:3000", "https://rmsx.herokuapp.com","https://dashboard.heroku.com/apps/rmsx"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:3001", "http://localhost:3000", "https://rmsx.herokuapp.com","http://rmsx.herokuapp.com","rmsx.herokuapp.com"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+//    CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("https://boot-hheroku.herokuapp.com"));
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+//        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+//                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+//                "Cache-Control", "Content-Type"));
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
