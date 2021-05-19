7f450ef846ba3a7a4602d4c8afa3d7637bb70261
smithtuka <smithtuka@gmail.com>
2021-05-19 00:18:13 -0500

    address stage toString

--- src/main/java/com/galbern/req/jpa/dao/RequisitionDao.java
+++ src/main/java/com/galbern/req/jpa/dao/RequisitionDao.java
@@ -18,6 +18,7 @@ public interface RequisitionDao extends JpaRepository<Requisition, Long> {
     List<Requisition> findRequisitionsByRequesterIdIn(List<Long> requesterIds);
     List<Requisition> findRequisitionsByApprovalStatus(ApprovalStatus approvalStatus);
     List<Requisition> findRequisitionsByRequiredDate(Date submissionDate);
+//    Query("SELECT r from Requisition r JOIN FETCH r.stage where r.id = :id")
     @Query("SELECT r.stage from Requisition r where r.id = :id")
     Stage getStageId(@Param("id") Long id);
 
